54a04a6416b6c2296119e8125be9dd41b827b959
hwen <hwendev@gmail.com>
2022-01-30 14:57:44 +0800

    uf

--- python/templates_graph.py
+++ python/templates_graph.py
@@ -20,6 +20,30 @@ class DSU:
     def disconnect(self, x):
         self.graph[x] = x
 
+
+class UnionFind:
+    def __init__(self):
+        self._parent = {}
+        self._size = {}
+
+    def union(self, a, b):
+        a, b = self.find(a), self.find(b)
+        if a == b:
+            return
+        if self._size[a] < self._size[b]:
+            a, b = b, a
+        self._parent[b] = a
+        self._size[a] += self._size[b]
+
+    def find(self, x):
+        if x not in self._parent:
+            self._parent[x] = x
+            self._size[x] = 1
+        while self._parent[x] != x:
+            self._parent[x] = self._parent[self._parent[x]]
+            x = self._parent[x]
+        return x
+
 # ----------------------------------- Bellman Ford -----------------------------------
 
 
