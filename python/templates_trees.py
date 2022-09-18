66223a908e8d491253e4476ae760e007357ae89e
houman <hwendev@gmail.com>
2022-09-18 12:31:22 +0800

    update trie tempalte

--- python/templates_trees.py
+++ python/templates_trees.py
@@ -41,7 +41,46 @@ class Trie:
 
         # assert word not in self  # confirm that the number has been removed
 
+# ----------------------------------- Trie -----------------------------------
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.hasEnd = False
 
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        
+        for x in word:
+            curr = curr.children[x]
+        
+        curr.hasEnd = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        
+        for x in word:
+            if x not in curr.children:
+                return False
+            
+            curr = curr.children[x]
+        
+        return curr.hasEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        
+        for x in prefix:
+            if x not in curr.children:
+                return False
+            
+            curr = curr.children[x]
+        
+        return True
+    
 # ----------------------------------- Segment Tree -----------------------------------
 class SegmentTree:
     # https://github.com/cheran-senthil/PyRival/blob/master/pyrival/data_structures/SegmentTree.py
