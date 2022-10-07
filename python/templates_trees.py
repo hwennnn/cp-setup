4357e0f33aeaaa26c4f24e5d28246208ac87bd6a
houman <hwendev@gmail.com>
2022-10-07 14:30:40 +0800

    update lazy seg tree

--- python/templates_trees.py
+++ python/templates_trees.py
@@ -114,7 +114,7 @@ class SegmentTree:
     def __len__(self):
         return self._len
 
-    def query(self, start, stop):
+    def query(self, start, stop): # stop not inclusive``
         """func of data[start, stop)"""
         start += self._size
         stop += self._size
@@ -134,3 +134,46 @@ class SegmentTree:
 
     def __repr__(self):
         return "SegmentTree({0})".format(self.data)
+
+class LazySegmentTree:
+    def __init__(self, low, high, val):
+        self.low = low
+        self.high = high
+        self.val = val
+        self.lazy = 0
+        self.left = None
+        self.right = None
+    
+    def _extend(self):
+        if self.low < self.high:
+            if self.left is None:
+                mid = (self.low + self.high) // 2
+                self.left = LazySegmentTree(self.low, mid, self.val)
+                if mid + 1 <= self.high:
+                    self.right = LazySegmentTree(mid + 1, self.high, self.val)
+            elif self.lazy != 0:
+                self.left.val += self.lazy
+                self.left.lazy += self.lazy
+                self.right.val += self.lazy
+                self.right.lazy += self.lazy
+        self.lazy = 0
+        
+    def update(self, low, high, delta): # [low, high] inclusive
+        if low > high or self.high < low or high < self.low:
+            return
+        if low <= self.low and self.high <= high:
+            self.val += delta
+            self.lazy += delta
+            return
+        self._extend()
+        self.left.update(low, high, delta)
+        self.right.update(low, high, delta)
+        self.val = max(self.left.val, self.right.val)
+    
+    def query(self, low, high):  # [low, high] inclusive
+        if low > high or self.high < low or high < self.low:
+            return 0
+        if low <= self.low and self.high <= high:
+            return self.val
+        self._extend()
+        return max(self.left.query(low, high), self.right.query(low, high))
